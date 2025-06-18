192313b32de554605f891ccf5e6075f8c32cd7d2
HorrorReaper <patrick.egi04@gmail.com>
2025-06-18 08:22:37 +0200

    Bootstrap eingefügt

--- src/gewohnheitstier/src/index.js
+++ src/gewohnheitstier/src/index.js
@@ -7,6 +7,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+    <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossorigin></script>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
+
+<script>var Alert = ReactBootstrap.Alert;</script>
     <App />
   </React.StrictMode>
 );
