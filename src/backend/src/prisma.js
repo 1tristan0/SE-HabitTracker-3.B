84c60db872d2042ca8bc4377932f2351d99f0d0c
Tristan Weiland <tristan.weiland2@stud.htw-dresden.de>
2025-12-14 16:37:47 +0100

    Requested Changes Update: Kommentare übersetzt, toten Code entfernt

--- src/backend/src/prisma.js
+++ src/backend/src/prisma.js
@@ -1,3 +1,4 @@
+// Rückfall auf Standard Location für den generierten Client, siehe Prisma Doku
 //const { PrismaClient } = require('./generated/prisma');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
