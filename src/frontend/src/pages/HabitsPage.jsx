12f1856b133c53861eddb24208a93281f6e95094
Tristan Weiland <tristan.weiland2@stud.htw-dresden.de>
2026-01-18 18:03:13 +0100

    rebase to dev: resolved all merge conflicts Frontend: HabitsBeareiten <-> Tier + Backend: Prisma + Habits API

--- src/frontend/src/pages/HabitsPage.jsx
+++ src/frontend/src/pages/HabitsPage.jsx
@@ -93,11 +93,6 @@ export default function HabitsPage({ session, onLogout }) {
     setSelectedBegleiter(data.animal_type);
     console.log("Begleiter gesetzt:", data);
   }
-  const setAnimal = async (animalType, animalmood) => {
-    const data = await setAnimal(token, animalType, animalmood);
-    setSelectedBegleiter(data.animal_type);
-    console.log("Begleiter gesetzt:", data);
-  }
 
   const opennModal = (habit) => {
     setSelectedHabit(habit);
