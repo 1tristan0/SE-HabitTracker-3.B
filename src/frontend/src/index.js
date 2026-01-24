634470300ccb9f21ef2c1c631e71e9d23932acb8
Tristan Weiland <tristan.weiland2@stud.htw-dresden.de>
2026-01-24 16:46:07 +0100

    Update: react-router-dom -> react-router

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router';
 
 // nur wenn du reportWebVitals weiter nutzen möchtest, diesen Import behalten
 // import reportWebVitals from './reportWebVitals';
--- src/frontend/src/pages/CreateHabitPage.jsx
+++ src/frontend/src/pages/CreateHabitPage.jsx
@@ -1,5 +1,5 @@
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import {
   addHabit as apiAddHabit,
 } from "../api/habitsApi";
