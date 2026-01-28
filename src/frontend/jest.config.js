b0563642ce52e86b7b30e8fc1945252a9cffce30
HorrorReaper <patrick.egi04@gmail.com>
2026-01-28 08:37:59 +0100

    Update jest config to reference new setupTests location

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  testMatch: ['tests/**/*.test.js', 'src/**/*.test.js'],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/index.js',
-    '!src/reportWebVitals.js',
-  ],
 };
